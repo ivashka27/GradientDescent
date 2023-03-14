355c162e1ab9d17af0e9359c8a362d101b6a13d4
Your Name <you@example.com>
2023-03-14 21:32:49 +0300

    drows function and gradient path plots, works

--- main.py
+++ main.py
@@ -4,28 +4,27 @@ import matplotlib.pyplot as plt
 
 # f(x, y) = ax^2 + by^2 + cxy + dx + ey + f
 # make_function() returns f(x, y) as a dictionary
-
 def make_function(a, b, c, d, e, f):
     return {'a': a, 'b': b, 'c': c, 'd': d, 'e': e, 'f': f}
 
 
-# return value of function
-def get_function_value(X, Y, function):
+# return list of function values
+def function_value_list(X, Y, function):
     res = []
     for i in range(len(X)):
         x = X[i]
         y = Y[i]
-        res += [function['a'] * x ** 2 +
-                function['b'] * y ** 2 +
-                function['c'] * x * y +
-                function['d'] * x +
-                function['e'] * y +
-                function['f']]
+        res += [get_current_value(x, y, function)]
     return res
 
 
-# calculate_gradient() returns gradient of the function
+# return current value of function
+def get_current_value(x, y, function):
+    return function['a'] * x ** 2 + function['b'] * y ** 2 + function['c'] * x * y + \
+           function['d'] * x + function['e'] * y + function['f']
 
+
+# calculate_gradient() returns gradient of the function
 def calculate_gradient(x, y, coefficient):
     return [
         2.0 * coefficient['a'] * x + coefficient['c'] * y + coefficient['d'],
@@ -45,41 +44,43 @@ def gradient_descent(start_x, start_y, function, learning_rate, eps):
     prev_y = start_y
     condition = 1
     res = []
-    points = []
+    res += [(start_x, start_y)]
 
     while condition:
         grad = calculate_gradient(prev_x, prev_y, function)
         new_x = prev_x - learning_rate * grad[0]
         new_y = prev_y - learning_rate * grad[1]
-        res += [(new_x, new_y)]
+        res += [[new_x, new_y]]
         if abs(prev_x - new_x) <= eps and abs(prev_y - new_y) <= eps:
             break
         prev_x = new_x
         prev_y = new_y
     return res
 
+
 f1 = make_function(1.0, 1.0, 0.0, 0.0, 0.0, 0.0)
 f2 = make_function(2.0, 6.0, 0.0, 15.0, 2.0, 0.0)
 
 # draw function graph
-fig = plt.figure()
-ax = plt.axes(projection='3d')
-t = np.linspace(-10, 10, 100)
+t = np.linspace(-20, 10, 100)
 X, Y = np.meshgrid(t, t)
-Z = np.array(get_function_value(X, Y, f2))
-surf = ax.plot_surface(X, Y, Z)
-plt.show()
-
-
-step = 0.06
-epsilon = 0.00001
+Z = np.array(function_value_list(X, Y, f2))
+fig, ax1 = plt.subplots(1)
+fig = plt.figure().add_subplot(projection='3d')
+fig.plot_surface(X, Y, Z)
+##
+step = 0.01
+epsilon = 0.001
 
 res1 = gradient_descent(-5.0, -5.0, f1, step, epsilon)
-res2 = gradient_descent(0.0, 0.0, f2, step, epsilon)
+res2 = gradient_descent(-5.0, -5.0, f2, step, epsilon)
+points = np.array(res2)
 # draw gradient move
-plt.plot(res1[:, 0], res1[:, 1], 'o-')
-plt.contour(X, Y, Z, levels=sorted([get_function_value(p[0], p[1], f1) for p in res1]))
+ax1.plot(points[:, 0], points[:, 1], 'o-')
+ax1.grid()
+ax1.contour(X, Y, Z, levels=sorted([get_current_value(p[0], p[1], f2) for p in points]))
+##
 
-
-print(res1)
-print(res2)
+# print(res1)
+# print(res2)
+plt.show()
