5d15ffd32c929fd0e3177a9387220331e4d42ed1
ivashka27 <ivan_palchenkov@mail.ru>
2023-03-14 16:56:40 +0300

    lol

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
 import numpy as np
 
 # f(x, y) = ax^2 + by^2 + cxy + dx + ey + f
+# make_function() returns f(x, y) as a dictionary
 
 
 def make_function(a, b, c, d, e, f):
     return {'a': a, 'b': b, 'c': c, 'd': d, 'e': e, 'f': f}
+
+
+# calculate_gradient() returns gradient of the function
 
 
 def calculate_gradient(x, y, coefficient):
@@ -32,7 +36,6 @@ def gradient_descent(start_x, start_y, function, learning_rate, eps):
         new_x = prev_x - learning_rate * grad[0]
         new_y = prev_y - learning_rate * grad[1]
         res += [(new_x, new_y)]
-        #print(prev_x, prev_y, new_x, new_y)
         if abs(prev_x - new_x) <= eps and abs(prev_y - new_y) <= eps:
             break
         prev_x = new_x
@@ -42,15 +45,12 @@ def gradient_descent(start_x, start_y, function, learning_rate, eps):
 
 f1 = make_function(1.0, 1.0, 0.0, 0.0, 0.0, 0.0)
 f2 = make_function(-2.0, -6.0, 0.0, 15.0, 2.0, 0.0)
-f3 = make_function(-1.0, 3.0, 1.0, 0.0, -5.0, 0.0)
 
 step = 0.06
 epsilon = 0.00001
 
-#res1 = gradient_descent(-5.0, -5.0, f1, step, epsilon)
+res1 = gradient_descent(-5.0, -5.0, f1, step, epsilon)
 res2 = gradient_descent(0.0, 0.0, f2, step, epsilon)
-#res3 = gradient_descent(0.0, 0.0, f3, step, epsilon)
 
+print(res1)
 print(res2)
-#print(res2)
-#print(res3)
